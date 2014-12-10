f8ad26e713462fdc37250011afee06909dcc31e8
zephyr_chen <zephyr_chen@gemteks.com>
2014-12-10 14:58:17 +0800

    add available_product api

---
+++ routes/billing/BillingAvailableProduct.js
@@ -0,0 +1,106 @@
+
+var billingPurchasingStat = require('./BillingPurchasingStat');
+var dotenv = require('dotenv').load();
+var google = require('googleapis');
+var androidpublisher = google.androidpublisher('v2');
+var OAuth2 = google.auth.OAuth2;
+var packageName  = process.env.PACKAGE_NAME;
+var clientID     = process.env.CLIENT_ID;
+var clientSecret = process.env.CLIENT_SECRET;
+var redirectURL  = process.env.REDIRECT_URL;
+var code         = process.env.CODE;
+var refreshToken = process.env.REFRESH_TOKEN;
+var oauth2Client = new OAuth2(clientID, clientSecret, redirectURL);
+
+var statusCode = 
+[
+	{
+		code: 1211, 
+		message: 'Query available product success'
+	}, 
+	{
+		code: 1400, 
+		message: 'Missing parameter'
+	}, 
+	{
+		code: 1411, 
+		message: 'No data found'
+	} 
+];
+
+var available_product = function(body, fail, pass) {
+	var access_token = body.access_token;
+	var packageName = body.package_name;
+	var ret;
+
+	if( access_token == null || access_token == '' || packageName == null || packageName == '' )
+	{
+		ret = {
+			status: statusCode[1], 
+		}
+		if(typeof fail == 'function' && fail != null ) 
+			fail(ret);
+		return;
+	}
+
+	oauth2Client.setCredentials({
+		token: access_token,
+		refresh_token: refreshToken
+	});
+
+	androidpublisher.inappproducts.list({
+		packageName: packageName,
+		auth: oauth2Client
+	}, function(err, response) {
+			if(err)
+			{
+				if(err.message.indexOf('Missing') >= 0)
+				{
+					ret = {
+						status: statusCode[1], 
+						data: err
+					}
+				}
+				else
+				{
+					ret = {
+						status: statusCode[2], 
+						data: err
+					}
+				}
+				if(typeof fail == 'function' && fail != null ) 
+					fail(ret);
+			}
+			else
+			{
+				//console.log(response);
+				var productList = [];
+				response.inappproduct.forEach(function(entry) {
+					productList.push(entry.sku);
+				});
+				var product = {
+					package_name: packageName, 
+					product_ID_list: productList
+				};
+				if( productList.length > 0 )
+				{
+					billingPurchasingStat.cancel();
+					ret = {
+						status: statusCode[0], 
+						product: product
+					}
+				}
+				else
+				{
+					ret = {
+						status: statusCode[2]
+					};
+				}
+				if(typeof pass == 'function' && pass != null ) 
+					pass(ret);
+				console.log(ret);
+			}
+		});
+}
+
+exports.module = available_product;
