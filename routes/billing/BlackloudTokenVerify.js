fa033c31a65ab441dc3f0b7b5815030ca4fa1d6c
roy_yang <roy_yang@gemteks.com>
2014-12-12 10:34:43 +0800

    Fix token verify function

--- routes/billing/BlackloudTokenVerify.js
+++ routes/billing/BlackloudTokenVerify.js
@@ -7,25 +7,24 @@ var env = dotEnv.parse(file);
 var BlackCloudLogger = require("../../utils/BlackloudLogger");
 var logger = BlackCloudLogger.new(env.PROJECT_NAME, "blackloudTokenVerify");
 
-exports.verify = function(token, fail, success) {
+exports.verify = function(token, pass, fail) {
     var params = {
         token: token,
         api_key: "ICE-F2aOPC64Ke",
         api_token: "CiMrSnX4rvuujJjhqLNy",
-        time: new time.Date()
+        time: Math.floor(new time.Date())
     }; 
-    
-    //process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
     request({
         method: "POST",
         headers: {"content-type" : "application/x-www-form-urlencoded"},
-        url:     "https://s5.securepilot.com/v1/service/token_verify",
-        json:    true,
-        body:    params
+        url:     "https://s5.securepilot.com/v1/token_verify",
+        body:    JSON.stringify(params)
     }, function(error, response, body) {
        BlackCloudLogger.log(logger, "info", "response.statusCode = " + response.statusCode);
+       BlackCloudLogger.log(logger, "info", "response.statusCode = " + response.body);
        if (!error && response.statusCode == 200)
-           success();
+           pass();
        else
            fail();
     });
