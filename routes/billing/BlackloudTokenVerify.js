d2557ddbb37e74b6a57b5dee1a5b7f5e071467eb
roy_yang <roy_yang@gemteks.com>
2014-12-16 14:36:38 +0800

    Modify token verify function

--- routes/billing/BlackloudTokenVerify.js
+++ routes/billing/BlackloudTokenVerify.js
@@ -8,21 +8,23 @@ var BlackCloudLogger = require("../../utils/BlackloudLogger");
 var logger = BlackCloudLogger.new(env.PROJECT_NAME, "blackloudTokenVerify");
 
 exports.verify = function(token, pass, fail) {
+    var current_time = Math.floor(new time.Date());
     var params = {
         token: token,
         api_key: "ICE-F2aOPC64Ke",
         api_token: "CiMrSnX4rvuujJjhqLNy",
-        time: Math.floor(new time.Date())
+        time: current_time
     }; 
 
     request({
         method: "POST",
-        headers: {"content-type" : "application/x-www-form-urlencoded"},
+        headers: {"content-type" : "application/json"},
         url:     "https://s5.securepilot.com/v1/token_verify",
-        body:    JSON.stringify(params)
+        json:    true,
+        body:    params
     }, function(error, response, body) {
-       BlackCloudLogger.log(logger, "info", "response.statusCode = " + response.statusCode);
-       BlackCloudLogger.log(logger, "info", "response.statusCode = " + response.body);
+       BlackCloudLogger.log(logger, "info", "statusCode = " + response.statusCode);
+       BlackCloudLogger.log(logger, "info", "body = " + body);
        if (!error && response.statusCode == 200)
            pass();
        else
