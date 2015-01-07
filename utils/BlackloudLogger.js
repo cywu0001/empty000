0014b7aaab1ba8f7e45bcbbea9972153e756c0b8
zephyr_chen <zephyr_chen@gemteks.com>
2015-01-07 15:47:36 +0800

    Modify BlackloudLogger API

--- utils/BlackloudLogger.js
+++ utils/BlackloudLogger.js
@@ -1,95 +1,71 @@
-var fs = require('fs');
-var dotenv = require('dotenv');
-var winston = require('winston');
+var	winston = require('winston'),
+	winston_es = require('winston-elasticsearch');
 var winstonNsSocket = require('winston-nssocket').Nssocket;
-//var log2s3 = require('./BlackloudLog2S3');
-var logSize = 1024 * 1024 * 1024;//10M
-/*
- * --------------------------------------------------------
- * Setup server log directory (Create if not exist)
- * --------------------------------------------------------
- */
-function checkDirExist(pid){
-	if(!fs.existsSync('log')){
-	  console.log("Log folder not exist, create it.\n");
-	  fs.mkdirSync('log', 0775, function(err){
-	    if(err){
-	      console.log("Log folder create failure.\n");
-	    }
-	  });
-	}
-	else if(!fs.existsSync('log/' + pid)){
-	  console.log(pid + " folder not exist, create it.\n");
-	  fs.mkdirSync('log/' + pid, 0775, function(err){
-	    if(err){
-	      console.log(pid +" folder create failure.\n");
-	    }
-	  });
+
+var logServerInfo = {
+	ip : '54.68.219.109', 
+	port : {
+		'WEATHER' : 8081,
+		'BILLING' : 8082,
+		'NONE3'   : 8083,
+		'NONE4'   : 8084,
+		'NONE5'   : 8085,
+		'NONE6'   : 8086,
+		'NONE7'   : 8087,
+		'NONE8'   : 8088,
+		'NONE9'   : 8089,
+		'NONE10'  : 8090
 	}
 }
 
-function getTimeStamp() {
-	var date = new Date();
-	var timeString = date.toLocaleTimeString();
-	var timeStamp = timeString;
-	//console.log('hello! -- timestamp function log -- ' + timeStamp);
-
-	return timeStamp;
-}
-
-var logServerIP = '54.68.219.109';
-var portMap = ({
-	'WEATHER': 8081,
-	'BILLING': 8082,
-});
-var BlackloudLogger = {
-	new: function(project, title) {
-		// create a folder for PROJECT_NAME, 
-		// to store the local exception files.
-		//checkDirExist(projectName);
-
-		var logger = new (winston.Logger)({
+function BlackloudLogger(project, title) {
+	this.project = project;
+    this.title = title ? title : null;
+	this.logger = 
+		new (winston.Logger)({
 			// default transports were set to the remote log server
 			transports: [
 			    new (winston.transports.Console)({ 
 					json: false, 
-					label: title,
+					label: this.title,
 					timestamp: true, 
 				}),
+				/*
+				new winston_es({ 
+					level     : 'info',
+					host      : logServerInfo.ip,
+					indexName : 'BlackloudLogger',
+					typeName  : this.project, 
+					diable_fields : true
+				})
+				*/
 			],
 			exceptionHandlers: [
 			    new (winston.transports.Console)({ 
 					json: false, 
-					label: title,
+					label: this.title,
 					timestamp: true 
 				}),
-/*
-				new winston.transports.DailyRotateFile({
-					name: 'file',
-					filename: __dirname + '/log/' + projectName + '/exceptions.log',
-					datePattern: '.yyyy-MM-dd',
-					label: title,
-					timestamp: function() { 
-						return getTimeStamp();
-					}
-				})
-*/
 			],
 			exitOnError: false, 
 		});
-		//console.log(map[project]);
-		logger.add(winstonNsSocket, {
-			host: logServerIP,
-			port: portMap[project]
-		});
-		return logger;
-	},
-	log: function(logger, type, data, metadata) {
-		if( metadata == null )
-			logger.log(type, data);
-		else
-			logger.log(type, data, metadata);
-	},
+	this.logger.add(winstonNsSocket, {
+		host: logServerInfo.ip,
+		port: logServerInfo.port[project]
+	});
+}
+
+BlackloudLogger.prototype = {
+    log	    : log,
+    setTitle: setTitle
+};
+
+function log(type, data) {
+	this.logger.log(type, data, { label: this.title });
+}
+
+function setTitle(title) {
+    this.title = title;
 }
 
 module.exports = BlackloudLogger;
